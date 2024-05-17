517029066835e1dfaddbca5b685295b8b9fc31ae
Spyingwind <spyingwind@gmail.com>
2024-05-17 10:00:25 -0500

    Only moves mouse one pixel in x direction quickly every 60s Fixes globbing Replaces ls with find

--- mouse.sh
+++ mouse.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-_dev=$(ls -1 /dev/input/by-id/*event-mouse | tail -1)
+_dev=$(find /dev/input/by-id/*event-mouse | tail -1)
 [ -z "${_dev}" ] && echo "Cannot find mouse event device" && exit 1
 echo "Using ${_dev}"
-while [ true ]
-do
-        sleep 1
-        /usr/bin/evemu-event ${_dev} --type EV_REL --code REL_X --value $(( $RANDOM % 50 + 1 - 25 )) --sync
-        /usr/bin/evemu-event ${_dev} --type EV_REL --code REL_Y --value $(( $RANDOM % 50 + 1 - 25 )) --sync
-        echo -n "#"
+while true; do
+    sleep 60
+    /usr/bin/evemu-event "${_dev}" --type EV_REL --code REL_X --value $((-1)) --sync
+    sleep 0.001
+    /usr/bin/evemu-event "${_dev}" --type EV_REL --code REL_X --value $((1)) --sync
+    echo -n "#"
 done
